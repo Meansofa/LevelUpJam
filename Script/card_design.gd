2a1c0c0a2a58bc36824d42b69fdc9e7bd1d0e490
Coretico <145187490+Meansofa@users.noreply.github.com>
2025-10-31 09:16:34 +0800

    separated card design from pick_card tscn, added some sprites

---
+++ Script/card_design.gd
@@ -0,0 +1,19 @@
+extends CanvasGroup
+
+@export var elixer_texture : CompressedTexture2D
+
+func update_visuals(card : piece):
+	%health_label.text = "[b]" + str(card.health)
+	%card_label.text = "[center]" + card.name
+	%damage_label.text = "[b]" + str(card.damage)
+	var player_id = multiplayer.get_unique_id()
+	if player_id == 1:
+		$%pawn_texture.texture_normal = card.player_pawn_texture
+	else: $%pawn_texture.texture_normal = card.opponent_pawn_texture
+	
+	for elixer in %elixer.get_children(): #Clear first before getting replaced
+		elixer.queue_free()
+	for elixer in range(card.elixer): #elixer/power needed for this card
+		var textureRect = TextureRect.new()
+		textureRect.texture = elixer_texture
+		%elixer.add_child(textureRect)
--- Script/pick_card.gd
+++ Script/pick_card.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 @onready var spawn_position = self.position
 
-@export var elixer_texture : CompressedTexture2D
+
 
 var dragging := false #if the card is getting dragged
 var in_area := false #if mouse is hovering in the card
@@ -35,24 +35,8 @@ func give_card(new_card : piece):
 	if new_card == null:
 		print(name, ": new_card is null")
 	card = new_card
-	update_visuals()
+	%card.update_visuals(card)
 #<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-
-func update_visuals():
-	%health_label.text = "[b]" + str(card.health)
-	%card_label.text = "[center]" + card.name
-	%damage_label.text = "[b]" + str(card.damage)
-	var player_id = multiplayer.get_unique_id()
-	if player_id == 1:
-		$%pawn_texture.texture_normal = card.player_pawn_texture
-	else: $%pawn_texture.texture_normal = card.opponent_pawn_texture
-	
-	for elixer in %elixer.get_children(): #Clear first before getting replaced
-		elixer.queue_free()
-	for elixer in range(card.elixer): #elixer/power needed for this card
-		var textureRect = TextureRect.new()
-		textureRect.texture = elixer_texture
-		%elixer.add_child(textureRect)
 
 func _input(event):
 	if event is InputEventMouseButton:
